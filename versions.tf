cf10f033d3ab8c9c547d62dfa9b96d41c12bc12d
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2022-07-10 01:57:38 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,24 +1,24 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "2.66.0"
-#     }
-#   }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
 
-#   required_version = ">= 0.14"
+  required_version = ">= 0.14"
   
-# }
-
-
-# Configure the Microsoft Azure Provider
-
-  provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id 
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id 
 }
+
+
+# # Configure the Microsoft Azure Provider
+
+#   provider "azurerm" {
+#   features {}
+
+#   subscription_id = var.subscription_id
+#   client_id       = var.client_id 
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id 
+# }
 
