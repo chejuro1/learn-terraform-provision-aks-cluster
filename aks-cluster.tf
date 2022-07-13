54d3b07ad0c3fde9622752e4770c0b85d62e771c
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2022-07-13 15:27:34 -0400

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -4,8 +4,8 @@ provider "azurerm" {
   features {}
   
  subscription_id = var.subscription_id
- client_id       = var.client_id 
- client_secret   = var.client_secret
+ client_id       = var.appId
+ client_secret   = var.password
  tenant_id       = var.tenant_id 
 }
 
