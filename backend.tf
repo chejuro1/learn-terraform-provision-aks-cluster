983dfac78c65af61bfb022b2a940f4038d1a8dbe
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2022-07-13 15:48:47 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "backendstoragec"
-    container_name       = "backend"
-    key                  = "terraform.tfstate"
-  }
-}
